748420d706d8d23fab7a47153fc9e285bde3a99a
Rafael Pacheco Méndez <rafy.pachecomendez@gmail.com>
2022-02-27 12:55:25 -0300

    remove this

--- device.mk
+++ device.mk
@@ -85,8 +85,3 @@ PRODUCT_PACKAGES_ENG += \
 # Apex libraries
 PRODUCT_COPY_FILES += \
     $(OUT_DIR)/target/product/$(PRODUCT_RELEASE_NAME)/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so
-
-#TWRP
-PRODUCT_COPY_FILES += \
-    device/lenovo/j606l/prebuilt/systemmanifest.xml:$(TARGET_COPY_OUT_RECOVERY)/root/system/manifest.xml \
-    device/lenovo/j606l/prebuilt/vendormanifest.xml:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/manifest.xml \
